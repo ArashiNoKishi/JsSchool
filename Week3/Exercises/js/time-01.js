8be28ce487ff65defaa229938f933a082a229ab1
ArashiNoKishi <arashinokishi@gmail.com>
2018-06-05 15:55:32 -0500

    added exercise 3

---
+++ Week3/Exercises/js/time-01.js
@@ -0,0 +1,20 @@
+(function () {
+  var target = new Date(2018,10,19,0,0,0,0).getTime();
+
+  var update = setInterval( function(){
+    var now = new Date().getTime();
+
+    var difference = target - now;
+
+    var days = Math.floor((difference / (1000*60*60*24)));
+    var hours = Math.floor((difference % (1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((difference % (1000*60*60))/(1000*60));
+    var seconds = Math.floor((difference % (1000*60))/1000);
+
+    document.getElementById('countdown').innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  },1000);
+
+
+
+
+})();
